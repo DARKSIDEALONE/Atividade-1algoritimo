8a32846f36c0741707825fb17bb3d51acbfdce9a
Gabriel <queirozg048@gmail.com>
2022-07-20 15:05:46 -0300

    primeiro commit

---
+++ Algoritimo-1exercico/src/Aulas/PrimeiroExercicio.java
@@ -0,0 +1,33 @@
+package Aulas;
+
+public class PrimeiroExercicio {
+    public static void main (String [] args){
+
+//
+//      int numero1 = 17;
+//      double numero2 = 17.25;
+//      char sexo = 'M';
+//      String nome = "Gabriel Queiroz";
+//      boolean verdadeiro = false;
+//      boolean falso = true;
+//
+//        System.out.println(nome);
+//        System.out.println(verdadeiro);
+//        System.out.println(sexo);
+//        System.out.println(falso);
+//        System.out.println(numero1);
+//        System.out.println(numero2);
+
+        int idade = 17;
+        double dinheiro = 17.25;
+        String nome = "Gabriel Queiroz";
+
+        System.out.println(nome);
+        System.out.println(idade);
+        System.out.println(dinheiro);
+
+        System.out.println(nome + " tem " +idade + " anos "+" e possui " + dinheiro + " Reais ");
+
+    }
+
+}
